f2d16b85191c8bedf1f7d9d096f445313785e34d
Sebastian Wilgosz <sebastian@wilgosz.pl>
2018-04-25 13:22:27 +0200

    Feat: Article's model validations
    Implemented basic validations for articles:
    - title presence
    - content presence
    - slug presence & uniqueness
    
    Also configured FactoryBot to not require class name calls.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :slug, presence: true, uniqueness: true
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#validations' do
+    it 'should test that the factory is valid' do
+      expect(build :article).to be_valid
+    end
+
+    it 'should validate the presence of the title' do
+      article = build :article, title: ''
+      expect(article).not_to be_valid
+      expect(article.errors.messages[:title]).to include("can't be blank")
+    end
+
+    it 'should validate the presence of the content' do
+      article = build :article, content: ''
+      expect(article).not_to be_valid
+      expect(article.errors.messages[:content]).to include("can't be blank")
+    end
+
+    it 'should validate the presence of the slug' do
+      article = build :article, slug: ''
+      expect(article).not_to be_valid
+      expect(article.errors.messages[:slug]).to include("can't be blank")
+    end
+
+    it 'should validate uniqueness of the slug' do
+      article = create :article
+      invalid_article = build :article, slug: article.slug
+      expect(invalid_article).not_to be_valid
+    end
+  end
 end
