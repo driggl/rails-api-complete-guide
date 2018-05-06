73a8ef72b8337adf925b3c9e352c68db8a076fd2
Sebastian Wilgosz <sebastian@wilgosz.pl>
2018-05-06 14:29:23 +0200

    Feat: Create new user from github

--- app/lib/user_authenticator.rb
+++ app/lib/user_authenticator.rb
@@ -4,10 +4,27 @@ class UserAuthenticator
   attr_reader :user
 
   def initialize(code)
-
+    @code = code
   end
 
   def perform
-    raise AuthenticationError
+    client = Octokit::Client.new(
+      client_id: ENV['GITHUB_CLIENT_ID'],
+      client_secret: ENV['GITHUB_CLIENT_SECRET']
+    )
+    token = client.exchange_code_for_token(code)
+    if token.try(:error).present?
+      raise AuthenticationError
+    else
+      user_client = Octokit::Client.new(
+        access_token: token)
+      user_data = user_client.user.to_h.
+        slice(:login, :avatar_url, :url, :name)
+      User.create(user_data.merge(provider: 'github'))
+    end
   end
+
+  private
+
+  attr_reader :code
 end
--- spec/lib/user_authenticator_spec.rb
+++ spec/lib/user_authenticator_spec.rb
@@ -2,13 +2,49 @@ require 'rails_helper'
 
 describe UserAuthenticator do
   describe '#perform' do
+    let(:authenticator) { described_class.new('sample_code') }
+
+    subject { authenticator.perform }
+
     context 'when code is incorrect' do
+      let(:error) {
+        double("Sawyer::Resource", error: "bad_verification_code")
+      }
+
+      before do
+        allow_any_instance_of(Octokit::Client).to receive(
+          :exchange_code_for_token).and_return(error)
+      end
+
       it 'should raise an error' do
-        authenticator = described_class.new('sample_code')
-        expect{ authenticator.perform }.to raise_error(
+        expect{ subject }.to raise_error(
           UserAuthenticator::AuthenticationError
         )
         expect(authenticator.user).to be_nil
+      end
+    end
+
+    context 'when code is correct' do
+      let(:user_data) do
+        {
+          login: 'jsmith1',
+          url: 'http://example.com',
+          avatar_url: 'http://example.com/avatar',
+          name: 'John Smith'
+        }
+      end
+
+      before do
+        allow_any_instance_of(Octokit::Client).to receive(
+          :exchange_code_for_token).and_return('validaccesstoken')
+
+        allow_any_instance_of(Octokit::Client).to receive(
+          :user).and_return(user_data)
+      end
+
+      it 'should save the user when does not exists' do
+        expect{ subject }.to change{ User.count }.by(1)
+        expect(User.last.name).to eq('John Smith')
       end
     end
   end
