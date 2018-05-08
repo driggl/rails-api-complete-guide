148672289b5dd754c0f9a777a734a1f159424b62
Sebastian Wilgosz <sebastian@wilgosz.pl>
2018-05-08 08:57:02 +0200

    Test: Shared examples for authentication error

--- spec/controllers/access_tokens_controller_spec.rb
+++ spec/controllers/access_tokens_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe AccessTokensController, type: :controller do
   describe '#create' do
-    context 'when invalid request' do
+    shared_examples_for "unauthorized_requests" do
       let(:error) do
         {
           "status" => "401",
@@ -11,8 +11,6 @@ RSpec.describe AccessTokensController, type: :controller do
           "detail" => "You must provide valid code in order to exchange it for token."
         }
       end
-
-      subject { post :create }
 
       it 'should return 401 status code' do
         subject
@@ -23,6 +21,25 @@ RSpec.describe AccessTokensController, type: :controller do
         subject
         expect(json['errors']).to include(error)
       end
+    end
+
+    context 'when no code provided' do
+      subject { post :create }
+      it_behaves_like "unauthorized_requests"
+    end
+
+    context 'when invalid code provided' do
+      let(:github_error) {
+        double("Sawyer::Resource", error: "bad_verification_code")
+      }
+
+      before do
+        allow_any_instance_of(Octokit::Client).to receive(
+          :exchange_code_for_token).and_return(github_error)
+      end
+
+      subject { post :create, params: { code: 'invalid_code' } }
+      it_behaves_like "unauthorized_requests"
     end
 
     context 'when success request' do
