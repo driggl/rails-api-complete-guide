6badffc356867d34cf7d6d8928abf7323c7f5afd
Sebastian Wilgosz <sebastian@wilgosz.pl>
2018-05-12 00:10:48 +0200

    Refactor: Move tests to globally accessible shared examples.

--- spec/controllers/access_tokens_controller_spec.rb
+++ spec/controllers/access_tokens_controller_spec.rb
@@ -2,27 +2,6 @@ require 'rails_helper'
 
 RSpec.describe AccessTokensController, type: :controller do
   describe 'POST #create' do
-    shared_examples_for "unauthorized_requests" do
-      let(:authentication_error) do
-        {
-          "status" => "401",
-          "source" => { "pointer" => "/code" },
-          "title" =>  "Authentication code is invalid",
-          "detail" => "You must provide valid code in order to exchange it for token."
-        }
-      end
-
-      it 'should return 401 status code' do
-        subject
-        expect(response).to have_http_status(401)
-      end
-
-      it 'should return proper error body' do
-        subject
-        expect(json['errors']).to include(authentication_error)
-      end
-    end
-
     context 'when no code provided' do
       subject { post :create }
       it_behaves_like "unauthorized_requests"
@@ -79,27 +58,17 @@ RSpec.describe AccessTokensController, type: :controller do
   end
 
   describe 'DELETE #destroy' do
-    context 'when invalid request' do
-      let(:authorization_error) do
-        {
-          "status" => "403",
-          "source" => { "pointer" => "/headers/authorization" },
-          "title" =>  "Not authorized",
-          "detail" => "You have no right to access this resource."
-        }
-      end
+    subject { delete :destroy }
 
-      subject { delete :destroy }
+    context 'when no authorization header provided' do
 
-      it 'should return 403 status code' do
-        subject
-        expect(response).to have_http_status(:forbidden)
-      end
+      it_behaves_like 'forbidden_requests'
+    end
 
-      it 'should return proper error json' do
-        subject
-        expect(json['errors']).to include(authorization_error)
-      end
+    context 'when invalid authorization header provided' do
+      before { request.headers['authorization'] = 'Invalid token' }
+
+      it_behaves_like 'forbidden_requests'
     end
 
     context 'when valid request' do
---
+++ spec/support/shared/json_errors.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+shared_examples_for "unauthorized_requests" do
+  let(:authentication_error) do
+    {
+      "status" => "401",
+      "source" => { "pointer" => "/code" },
+      "title" =>  "Authentication code is invalid",
+      "detail" => "You must provide valid code in order to exchange it for token."
+    }
+  end
+
+  it 'should return 401 status code' do
+    subject
+    expect(response).to have_http_status(401)
+  end
+
+  it 'should return proper error body' do
+    subject
+    expect(json['errors']).to include(authentication_error)
+  end
+end
+
+shared_examples_for 'forbidden_requests' do
+  let(:authorization_error) do
+    {
+      "status" => "403",
+      "source" => { "pointer" => "/headers/authorization" },
+      "title" =>  "Not authorized",
+      "detail" => "You have no right to access this resource."
+    }
+  end
+
+  it 'should return 403 status code' do
+    subject
+    expect(response).to have_http_status(:forbidden)
+  end
+
+  it 'should return proper error json' do
+    subject
+    expect(json['errors']).to include(authorization_error)
+  end
+end
