914b1bdb022716dcb1dbdc38a6e84cfca0bb5d1a
Sebastian Wilgosz <sebastian@wilgosz.pl>
2018-05-18 21:31:21 +0200

    Feat: Articles#create handle validation errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,19 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    article = Article.new(article_params)
+    if article.valid?
+      #we will figure that out
+    else
+      render json: article, adapter: :json_api,
+        serializer: ActiveModel::Serializer::ErrorSerializer,
+        status: :unprocessable_entity
+    end
+  end
 
+  private
+
+  def article_params
+    ActionController::Parameters.new
   end
 end
--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -69,8 +69,51 @@ describe ArticlesController do
       it_behaves_like 'forbidden_requests'
     end
 
-    context 'when invalid parameters provided' do
+    context 'when authorized' do
+      let(:access_token) { create :access_token }
+      before { request.headers['authorization'] = "Bearer #{access_token.token}" }
 
+      context 'when invalid parameters provided' do
+        let(:invalid_attributes) do
+          {
+            data: {
+              attributes: {
+                tittle: '',
+                content: ''
+              }
+            }
+          }
+        end
+
+        subject { post :create, params: invalid_attributes }
+
+        it 'should return 422 status code' do
+          subject
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+
+        it 'should return proper error json' do
+          subject
+          expect(json['errors']).to include(
+            {
+              "source" => { "pointer" => "/data/attributes/title" },
+              "detail" =>  "can't be blank"
+            },
+            {
+              "source" => { "pointer" => "/data/attributes/content" },
+              "detail" =>  "can't be blank"
+            },
+            {
+              "source" => { "pointer" => "/data/attributes/slug" },
+              "detail" =>  "can't be blank"
+            }
+          )
+        end
+      end
+
+      context 'when success request sent' do
+
+      end
     end
   end
 end
