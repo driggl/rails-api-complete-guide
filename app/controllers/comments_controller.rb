9fff4e826e467d280a3c711e3077566d281de903
Sebastian Wilgosz <sebastian@wilgosz.pl>
2018-05-20 13:16:56 +0200

    Fix: List comments fix

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
   before_action :load_article
 
   def index
-    comments = @article.comments
+    comments = @article.comments.
       page(params[:page]).
       per(params[:per_page])
     render json: comments
