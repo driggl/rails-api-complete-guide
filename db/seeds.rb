e2876327bd16f0fbbc26e1c4201d9b0b0d4e91f0
Sebastian Wilgosz <sebastian@wilgosz.pl>
2018-05-20 13:17:16 +0200

    Chore: Seed improvements

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
+user = User.create(
+  { login: 'jsmith', name: 'John Smith', provider: 'github' }
+)
 Article.create([
-  { title: 'Article title 1', content: 'Article content 1', slug: 'article-title-1' },
-  { title: 'Article title 2', content: 'Article content 2', slug: 'article-title-2' },
-  { title: 'Article title 3', content: 'Article content 3', slug: 'article-title-3' }
+  { title: 'Article title 1',
+    content: 'Article content 1',
+    slug: 'article-title-1',
+    user: user
+  },
+  { title: 'Article title 2',
+    content: 'Article content 2',
+    slug: 'article-title-2',
+    user: user
+  },
+  { title: 'Article title 3',
+    content: 'Article content 3',
+    slug: 'article-title-3',
+    user: user
+  }
 ])
