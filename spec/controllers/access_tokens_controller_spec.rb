52727669132c26155facee05a8f37545fce2ad9f
Sebastian Wilgosz <sebastian@wilgosz.pl>
2018-08-23 09:26:47 +0200

    Tests: Access tokens test coverage for standard login support

--- spec/controllers/access_tokens_controller_spec.rb
+++ spec/controllers/access_tokens_controller_spec.rb
@@ -2,9 +2,54 @@ require 'rails_helper'
 
 RSpec.describe AccessTokensController, type: :controller do
   describe 'POST #create' do
+    let(:params) do
+      {
+        data: {
+          attributes: { login: "jsmith", password: "secret" }
+        }
+      }
+    end
+
     context 'when no auth_data provided' do
       subject { post :create }
       it_behaves_like "unauthorized_standard_requests"
+    end
+
+    context "when invalid login provided" do
+      let(:user) { create :user, login: 'invalid', password: 'secret' }
+      subject { post :create, params: params }
+
+      before { user }
+
+      it_behaves_like "unauthorized_standard_requests"
+    end
+
+    context "when invalid password provided" do
+      let(:user) { create :user, login: 'jsmith', password: 'invalid' }
+      subject { post :create, params: params }
+
+      before { user }
+
+      it_behaves_like "unauthorized_standard_requests"
+    end
+
+    context "when valid data provided" do
+      let(:user) { create :user, login: 'jsmith', password: 'secret' }
+      subject { post :create, params: params }
+
+      before { user }
+
+      it 'should return 201 status code' do
+        subject
+        expect(response).to have_http_status(:created)
+      end
+
+      it 'should return proper json body' do
+        subject
+        expect(json_data['attributes']).to eq(
+          { 'token' => user.access_token.token }
+        )
+      end
     end
 
     context 'when invalid code provided' do
