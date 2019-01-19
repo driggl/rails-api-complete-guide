bae99a3a13ef4b872e3d314e9bde077a1a969afb
Sebastian Wilgosz <sebastian@wilgosz.pl>
2019-01-19 12:20:08 +0100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -40,13 +40,6 @@ ActiveRecord::Schema.define(version: 2018_08_07_150152) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table "titles", force: :cascade do |t|
-    t.text "content"
-    t.string "slug"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "login", null: false
     t.string "name"
